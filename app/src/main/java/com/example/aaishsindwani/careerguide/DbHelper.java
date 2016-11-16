37c03a9377de602e3cedbf365b0a5d41fa6e3946
Aaish Sindwani <aaishsindwani@Aaishs-MacBook-Air.local>
2016-11-16 15:13:37 +0530

    timer commit(4)

--- app/src/main/java/com/example/aaishsindwani/careerguide/DbHelper.java
+++ app/src/main/java/com/example/aaishsindwani/careerguide/DbHelper.java
@@ -24,6 +24,10 @@ public class DbHelper extends SQLiteOpenHelper {
 	private static final String KEY_OPTC= "optc"; //option c
 	private static final String KEY_OPTD= "optd"; //option c
 	private static final String BRANCH="branch";
+	private static final String engg="ENGG";
+	private static final String medi="MEDI";
+	private static final String comm="COMM";
+	private static final String arts="ARTS";
 	private SQLiteDatabase dbase;
 	public DbHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -39,28 +43,95 @@ public class DbHelper extends SQLiteOpenHelper {
 		addQuestions();
 		//db.close();
 	}
-	private void addQuestions()
-	{
-		Question q1=new Question("Cricket in colonial India was organised on the " +
-				"principle of:","Religion", "Region", "Religion and Region", "National Loyalties","3","ARTS");
+	private void addQuestions() {
+		Question q1 = new Question("Cricket in colonial India was organised on the " +
+				"principle of:", "Religion", "Region", "Religion and Region", "National Loyalties", "3", arts);
 		this.addQuestion(q1);
-		Question q2=new Question("A rational number can be expresses as terminating decimal if the denominator has " +
-				"factors", "2,3 or 5", "2 or 5", "2 or 3","3 or 5", "2","ENGG");
+		Question q2 = new Question("A rational number can be expresses as terminating decimal if the denominator has " +
+				"factors", "2,3 or 5", "2 or 5", "2 or 3", "3 or 5", "2", engg);
 		this.addQuestion(q2);
-		Question q3=new Question("Under law of demand:","Price of commodity is an independent variable","" +
-				"Quantity demanded is a dependent variable","Reciprocal relationship is found between price and quantity " +
-				"demanded", "All of the above","4","COMM");
+		Question q3 = new Question("Under law of demand:", "Price of commodity is an independent variable", "" +
+				"Quantity demanded is a dependent variable", "Reciprocal relationship is found between price and quantity " +
+				"demanded", "All of the above", "4", comm);
 		this.addQuestion(q3);
-		Question q4=new Question("Plants synthesis protein from:","starch","sugar","amino acids","fatty acids","3","MED");
+		Question q4 = new Question("Plants synthesis protein from:", "starch", "sugar", "amino acids", "fatty acids", "3", medi);
 		this.addQuestion(q4);
-		Question q5=new Question("5. Most fish do not sink in water because of the presence of\n " +
+		Question q5 = new Question("5. Most fish do not sink in water because of the presence of\n " +
 				"I.   swim bladder\n" +
 				"II.  air bladder\n" +
 				"III. air sacs\n" +
 				"IV. air in spongy bones", "I and II are correct", "II and III are correct",
-				"III and IV are correct", "I, II, III and IV are correct","1","MED");
+				"III and IV are correct", "I, II, III and IV are correct", "1", medi);
 		this.addQuestion(q5);
-		}
+		Question q6 = new Question("The compound interest on Rs. 7500 at 4% per annum for 2 years compounded annually is",
+				"Rs. 610", "Rs. 612", "Rs. 614", "Rs. 616", "2", comm);
+		this.addQuestion(q6);
+		Question q7 = new Question("Democracy does not ensure:", "equality before the law of the State", "equal" +
+				" income to all citizens", "equality of opportunities to all citizens", "equal political rights" +
+				" to all citizens", "2", arts);
+		this.addQuestion(q7);
+		Question q8 = new Question("The tops of two poles of height 10m and 18m are connected with wire, " +
+				"If wire makes an angle of 30(degree) with horizontal, the the length of wire is",
+				"10m", "12m", "16m", "18m", "3", engg);
+		this.addQuestion(q8);
+		Question q9 = new Question("\"I am primarily interested in thinking processes; I am a __________ psychologist.\""
+				, "cognitive", "learning", "perception", "personality", "1", arts);
+		this.addQuestion(q9);
+		Question q10 = new Question("James borrows $10,000 from the bank. By the time the loan is repaid, James has paid the " +
+				"bank $10,400. What does the additional $400 represent?", "A Inflation rate", "Purchasing power of money"
+				, "Interest", "Investment", "3", comm);
+		this.addQuestion(q10);
+		Question q11 = new Question("Pine, fir, spruce, cedar, larch and cypress are the famous timber-yielding" +
+				" plants of which several also occur widely in the hilly regions of India. All these belong to:",
+				"Gymnosperms", "Angiosperms", "Monocotyledons", "Dicotyledons", "1", medi);
+		this.addQuestion(q11);
+		Question q12 = new Question("A farmer connects his electrical water pump in the field to a power" +
+				"socket in his home through a 500 m long cable. Which of the following" +
+				"statements is/are true ?\n" +
+				"I. 'rhe voltage across the pump is exactly equal to the voltage across" +
+				"the power socket at his home.\n" +
+				"II. The current flowing through the pump is exactly equal to the" +
+				"current flowing through the power socket at his home.", "Only 1 is correct"
+				, "Only II is correct", "Both I and II are correct", "Neither I nor II is correct ", "2", engg);
+		this.addQuestion(q12);
+		Question q13 = new Question("Passive factor of production is:", "Only land", "Only capital"
+				, "Neither land and capital", "Both land nor capital", "4", comm);
+		this.addQuestion(q13);
+		Question q14 = new Question("Which of the following is present in spinach but completely absent in" +
+				"butter made at home from milk ?",
+				"Fats", "Proteins", "Carbohydrates", "Dietary fibre", "4", medi);
+		this.addQuestion(q14);
+		Question q15 = new Question("Which one of the following tributaries meets·the Ganga river on its right bank?",
+				"Yamuna", "Gandak", "Ghaghara", "Kosi", "1", arts);
+		this.addQuestion(q15);
+		Question q16 = new Question("You feel more comfortable sitting on a cushioned sofa than on a hard" +
+				"wooden chair because compared to the chair, the sofa exerts",
+				"less force and less pressure on you.",
+				"less force but same pressure on you.",
+				"greater force but less pressure on you", "the same force but less pressure on you", "4", engg);
+		this.addQuestion(q16);
+		Question q17 = new Question("For inferior commodities, income effect is:",
+				"Negative", "Zero", "Infinite", "Positive", "1", comm);
+		this.addQuestion(q17);
+		Question q18 = new Question("Haemoglobin is a pigment that is present in the blood and carnes" +
+				"oxygen to all the tissues of the body. Anemia is a Condition in which" +
+				"the total haemoglobin content of the blood decreases. Which of the" +
+				"following will be the result of anemia ?",
+				"Increase in heart rate and increase in respiratory rate.",
+				"Decrease in heart rate ·and increase in respiratory rate.",
+				"Increase in heart rate and decrease in respiratory rate.",
+				"Increase in respiratory rate but heart rate unaffected.", "1", medi);
+		this.addQuestion(q18);
+		Question q19 = new Question("What led to the Civil Disobedience Movement?"
+				, "Arrival of the Simon Commission",
+				"Working at the firm without payments",
+				"Violation of Salt Tax by Gandhi",
+				"Fall in demand for agricultural goods", "3", arts);
+		this.addQuestion(q19);
+		Question q20 = new Question("If the sum of a positive number x and its reciprocal is twice their " +
+				"difference, x is:", "3", "1/3", "1/\u221A3", "\u221A3", "4", engg);
+		this.addQuestion(q20);
+	}
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldV, int newV) {
 		// Drop older table if existed
--- app/src/main/java/com/example/aaishsindwani/careerguide/QuizActivity.java
+++ app/src/main/java/com/example/aaishsindwani/careerguide/QuizActivity.java
@@ -1,22 +1,32 @@
 package com.example.aaishsindwani.careerguide;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toolbar;
 
+import com.serhatsurguvec.continuablecirclecountdownview.ContinuableCircleCountDownView;
+
 import java.util.List;
 
 public class QuizActivity extends Activity {
 	List<Question> quesList;
+	CountDownTimer cdt;
 	int score=0;
 	int qid=0;
 	Question currentQ;
@@ -24,10 +34,12 @@ public class QuizActivity extends Activity {
 	Button rda, rdb, rdc, rdd;
 	Button butNext;
 	String selected;
+	ContinuableCircleCountDownView mCountDownView;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_quiz);
+		mCountDownView = (ContinuableCircleCountDownView) findViewById(R.id.circleCountDownView);
 		//setTitle("Academic Quiz");
 		DbHelper db=new DbHelper(this);
 		quesList=db.getAllQuestions();
@@ -37,13 +49,39 @@ public class QuizActivity extends Activity {
 		rdb=(Button)findViewById(R.id.radio1);
 		rdc=(Button)findViewById(R.id.radio2);
 		rdd=(Button)findViewById(R.id.radio3);
+		mCountDownView.setTimer(15000);
+		mCountDownView.start();
+		mCountDownView.setListener(new ContinuableCircleCountDownView.OnCountDownCompletedListener() {
+			@Override
+			public void onTick(long passedMillis) {
+
+			}
+
+			@Override
+			public void onCompleted() {
+				Intent intent = new Intent(QuizActivity.this, ResultActivity.class);
+				Bundle b = new Bundle();
+				b.putInt("score", score); //Your score
+				intent.putExtras(b); //Put your score to your next Intent
+				startActivity(intent);
+				finish();
+			}
+		});
 		rda.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+					rda.setBackground(getResources().getDrawable(R.drawable.selected_btn));
+					rdb.setBackground(getResources().getDrawable(R.drawable.button));
+					rdc.setBackground(getResources().getDrawable(R.drawable.button));
+					rdd.setBackground(getResources().getDrawable(R.drawable.button));
+				}
+				else{
 				rda.setBackgroundColor(getResources().getColor(R.color.regbg));
 				rdb.setBackgroundColor(getResources().getColor(R.color.white_col));
 				rdc.setBackgroundColor(getResources().getColor(R.color.white_col));
 				rdd.setBackgroundColor(getResources().getColor(R.color.white_col));
+				}
 				rda.setTextColor(Color.WHITE);
 				rdb.setTextColor(getResources().getColor(R.color.bgColor));
 				rdc.setTextColor(getResources().getColor(R.color.bgColor));
@@ -55,10 +93,18 @@ public class QuizActivity extends Activity {
 		rdb.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				rdb.setBackgroundColor(getResources().getColor(R.color.regbg));
-				rda.setBackgroundColor(getResources().getColor(R.color.white_col));
-				rdc.setBackgroundColor(getResources().getColor(R.color.white_col));
-				rdd.setBackgroundColor(getResources().getColor(R.color.white_col));
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+					rdb.setBackground(getResources().getDrawable(R.drawable.selected_btn));
+					rda.setBackground(getResources().getDrawable(R.drawable.button));
+					rdc.setBackground(getResources().getDrawable(R.drawable.button));
+					rdd.setBackground(getResources().getDrawable(R.drawable.button));
+				}
+				else {
+					rdb.setBackgroundColor(getResources().getColor(R.color.regbg));
+					rda.setBackgroundColor(getResources().getColor(R.color.white_col));
+					rdc.setBackgroundColor(getResources().getColor(R.color.white_col));
+					rdd.setBackgroundColor(getResources().getColor(R.color.white_col));
+				}
 				rdb.setTextColor(Color.WHITE);
 				rda.setTextColor(getResources().getColor(R.color.bgColor));
 				rdd.setTextColor(getResources().getColor(R.color.bgColor));
@@ -70,10 +116,18 @@ public class QuizActivity extends Activity {
 		rdc.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				rdc.setBackgroundColor(getResources().getColor(R.color.regbg));
-				rdb.setBackgroundColor(getResources().getColor(R.color.white_col));
-				rda.setBackgroundColor(getResources().getColor(R.color.white_col));
-				rdd.setBackgroundColor(getResources().getColor(R.color.white_col));
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+					rdc.setBackground(getResources().getDrawable(R.drawable.selected_btn));
+					rdb.setBackground(getResources().getDrawable(R.drawable.button));
+					rda.setBackground(getResources().getDrawable(R.drawable.button));
+					rdd.setBackground(getResources().getDrawable(R.drawable.button));
+				}
+				else {
+					rdc.setBackgroundColor(getResources().getColor(R.color.regbg));
+					rdb.setBackgroundColor(getResources().getColor(R.color.white_col));
+					rda.setBackgroundColor(getResources().getColor(R.color.white_col));
+					rdd.setBackgroundColor(getResources().getColor(R.color.white_col));
+				}
 				rdc.setTextColor(Color.WHITE);
 				rda.setTextColor(getResources().getColor(R.color.bgColor));
 				rdb.setTextColor(getResources().getColor(R.color.bgColor));
@@ -85,10 +139,18 @@ public class QuizActivity extends Activity {
 		rdd.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				rdd.setBackgroundColor(getResources().getColor(R.color.regbg));
-				rdb.setBackgroundColor(getResources().getColor(R.color.white_col));
-				rdc.setBackgroundColor(getResources().getColor(R.color.white_col));
-				rda.setBackgroundColor(getResources().getColor(R.color.white_col));
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+					rdd.setBackground(getResources().getDrawable(R.drawable.selected_btn));
+					rdb.setBackground(getResources().getDrawable(R.drawable.button));
+					rdc.setBackground(getResources().getDrawable(R.drawable.button));
+					rda.setBackground(getResources().getDrawable(R.drawable.button));
+				}
+				else{
+					rdd.setBackgroundColor(getResources().getColor(R.color.regbg));
+					rdb.setBackgroundColor(getResources().getColor(R.color.white_col));
+					rdc.setBackgroundColor(getResources().getColor(R.color.white_col));
+					rda.setBackgroundColor(getResources().getColor(R.color.white_col));
+				}
 				rdd.setTextColor(Color.WHITE);
 				rdb.setTextColor(getResources().getColor(R.color.bgColor));
 				rda.setTextColor(getResources().getColor(R.color.bgColor));
@@ -103,6 +165,22 @@ public class QuizActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				RadioGroup grp=(RadioGroup)findViewById(R.id.radioGroup1);
+				if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+					rdd.setBackground(getResources().getDrawable(R.drawable.button));
+					rdb.setBackground(getResources().getDrawable(R.drawable.button));
+					rdc.setBackground(getResources().getDrawable(R.drawable.button));
+					rda.setBackground(getResources().getDrawable(R.drawable.button));
+				}
+				else{
+					rdd.setBackgroundColor(getResources().getColor(R.color.white_col));
+					rdb.setBackgroundColor(getResources().getColor(R.color.white_col));
+					rdc.setBackgroundColor(getResources().getColor(R.color.white_col));
+					rda.setBackgroundColor(getResources().getColor(R.color.white_col));
+				}
+				rdd.setTextColor(getResources().getColor(R.color.bgColor));
+				rdb.setTextColor(getResources().getColor(R.color.bgColor));
+				rda.setTextColor(getResources().getColor(R.color.bgColor));
+				rdc.setTextColor(getResources().getColor(R.color.bgColor));
 				//RadioButton answer=(RadioButton)findViewById(grp.getCheckedRadioButtonId());
 				//Log.d("yourans", currentQ.getANSWER()+" "+answer.getText());
 				if(currentQ.getANSWER().equals(selected))
@@ -121,6 +199,7 @@ public class QuizActivity extends Activity {
 					startActivity(intent);
 					finish();
 				}
+				selected="0";
 			}
 		});
 	}
@@ -140,6 +219,44 @@ public class QuizActivity extends Activity {
 		qid++;
 	}
 
+	private void start(){
+		cdt=new CountDownTimer(15 * 1000, 1000) {
+			@Override
+			public void onTick(long millisUntilFinished) {
+
+			}
+
+			@Override
+			public void onFinish() {
+
+			}
+		};
+
+	}
+
+	@Override
+	public void onBackPressed() {
+		AlertDialog.Builder abd = new AlertDialog.Builder(QuizActivity.this);
+		abd.setTitle("Exit Quiz Confirmation");
+		abd.setIcon(R.mipmap.quiz_exit);
+		abd.setMessage("Do you want to exit this Quiz");
+		abd.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i) {
+				mCountDownView.cancel();
+				Intent back=new Intent(QuizActivity.this,Home.class);
+				startActivity(back);
+				finish();
+			}
+		});
+		abd.setNegativeButton("No", new DialogInterface.OnClickListener() {
+			@Override
+			public void onClick(DialogInterface dialogInterface, int i) {
+
+			}
+		});
+		abd.show();
+	}
 	/*public void showlogo(){
 		getActionBar().setDisplayShowHomeEnabled(true);
 		getActionBar().setLogo(R.mipmap.ic_launcher);
